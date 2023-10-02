89e7f611ac5fb97fed19566dfb0d27805067dd54
brionnevi <victor.brionne@gmail.com>
2023-10-02 15:02:24 +0200

    Update entities using jhipster JDL

--- src/main/webapp/app/entities/order/order.test-samples.ts
+++ src/main/webapp/app/entities/order/order.test-samples.ts
@@ -6,24 +6,24 @@ import { IOrder, NewOrder } from './order.model';
 
 export const sampleWithRequiredData: IOrder = {
   id: 47761,
-  date: dayjs('2023-09-28T08:46'),
+  date: dayjs('2023-10-02T04:31'),
   state: State['CANCELLED'],
 };
 
 export const sampleWithPartialData: IOrder = {
   id: 52696,
-  date: dayjs('2023-09-28T00:28'),
+  date: dayjs('2023-10-01T20:13'),
   state: State['SIGNED'],
 };
 
 export const sampleWithFullData: IOrder = {
   id: 47091,
-  date: dayjs('2023-09-28T03:28'),
+  date: dayjs('2023-10-01T23:13'),
   state: State['DONE'],
 };
 
 export const sampleWithNewData: NewOrder = {
-  date: dayjs('2023-09-28T14:26'),
+  date: dayjs('2023-10-02T10:11'),
   state: State['CANCELLED'],
   id: null,
 };
