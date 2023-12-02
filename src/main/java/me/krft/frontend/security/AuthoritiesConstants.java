cb57b57617dd006a9442e195b8f1b8d3ce4a34be
brionnevi <victor.brionne@gmail.com>
2023-12-02 19:40:15 +0100

    19-create-authorities ## Created new authorities in AuthoritiesConstants.java and in authority.csv

--- src/main/java/me/krft/frontend/security/AuthoritiesConstants.java
+++ src/main/java/me/krft/frontend/security/AuthoritiesConstants.java
@@ -7,6 +7,10 @@ public final class AuthoritiesConstants {
 
     public static final String ADMIN = "ROLE_ADMIN";
 
+    public static final String MODERATOR = "ROLE_MODERATOR";
+
+    public static final String MERCHANT = "ROLE_MERCHANT";
+
     public static final String USER = "ROLE_USER";
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
